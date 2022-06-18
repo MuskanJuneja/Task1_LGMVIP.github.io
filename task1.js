1417629c6a72d338ea101daa42c0499de3b2a70f
MuskanJuneja <89866638+MuskanJuneja@users.noreply.github.com>
2022-06-18 10:48:00 +0530

    Task-TodoListApp
    This task is done in order to explore JavaScript features and complete LGM virtual internship.
    Task 1-Beginner Task-TodoListApp

---
+++ task1.js
@@ -0,0 +1,42 @@
+function addTask() {
+    let Taskname = document.getElementById("taskinp").value;
+    if (Taskname == "") {
+        alert("Please enter a task to continue");
+    }
+    else {
+        let list = document.getElementById("list");
+        let item = document.createElement('li');
+        let a = document.createElement('a');
+        let b = document.createElement('a');
+        a.className = 'fa-solid fa-xmark';
+        b.className = 'fa-solid fa-check';
+        item.textContent = Taskname;
+        a.style.textDecoration = 'none';
+        a.style.color = 'red';
+        b.style.color = 'green';
+        b.style.textDecoration = 'none';
+        item.appendChild(a);
+        item.appendChild(b);
+        let first = list.firstElementChild;
+        if (first == null) {
+            list.appendChild(item);
+            document.getElementById("taskinp").value = "";
+        }
+        else {
+            list.insertBefore(item, first);
+            document.getElementById("taskinp").value = "";
+        }
+        let del = a;
+        del.addEventListener('click', function (e) {
+            let list = document.getElementById("list");
+            let item = e.target.parentNode;
+            list.removeChild(item);
+        })
+        let done = b;
+        done.addEventListener('click', function (e) {
+            let item = e.target.parentNode;
+            item.style.color = 'red';
+            item.style.textDecoration = 'line-through';
+        })
+    }
+}
